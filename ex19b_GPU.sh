39a6a73cc781ef45ec2eb708e025096946a1e826
Ben Kirk <benkirk@ucar.edu>
2022-09-29 16:22:42 -0600

    additional ex19 permutations

--- ex19b_GPU.sh
+++ ex19b_GPU.sh
@@ -2,7 +2,7 @@
 #PBS -A <project_code>
 #PBS -j oe
 #PBS -l walltime=00:30:00
-#PBS -l select=1:ncpus=64:mpiprocs=64:ompthreads=1:ngpus=4
+#PBS -l select=2:ncpus=64:mpiprocs=64:ompthreads=1:ngpus=4
 
 ### Set temp to scratch
 [ -d /glade/gust/scratch/${USER} ] && export TMPDIR=/glade/gust/scratch/${USER}/tmp && mkdir -p $TMPDIR
@@ -40,12 +40,21 @@ echo "------------------------------------------------"
 ldd ex19
 
 echo && echo && echo "********* Intra-Node (CPU) *****************"
+mpiexec -n 32 --ppn 32 \
+        ./ex19 -cuda_view -snes_monitor -pc_type mg -da_refine 10 -snes_view -pc_mg_levels 9 -mg_levels_ksp_type chebyshev -mg_levels_pc_type jacobi -log_view \
+    || status="FAIL"
+
 mpiexec -n 64 --ppn 64 \
         ./ex19 -cuda_view -snes_monitor -pc_type mg -da_refine 10 -snes_view -pc_mg_levels 9 -mg_levels_ksp_type chebyshev -mg_levels_pc_type jacobi -log_view \
     || status="FAIL"
 
 echo && echo && echo "********* Intra-Node (GPU) *****************"
 mpiexec -n 4 --ppn 4 ${top_dir}/get_local_rank \
+        ./ex19 -cuda_view -snes_monitor -pc_type mg -dm_mat_type aijcusparse -dm_vec_type cuda -da_refine 10 -snes_view -pc_mg_levels 9 -mg_levels_ksp_type chebyshev -mg_levels_pc_type jacobi -log_view \
+    || status="FAIL"
+
+echo && echo && echo "********* Inter-Node (GPU) *****************"
+mpiexec -n 8 --ppn 4 ${top_dir}/get_local_rank \
         ./ex19 -cuda_view -snes_monitor -pc_type mg -dm_mat_type aijcusparse -dm_vec_type cuda -da_refine 10 -snes_view -pc_mg_levels 9 -mg_levels_ksp_type chebyshev -mg_levels_pc_type jacobi -log_view \
     || status="FAIL"
 
